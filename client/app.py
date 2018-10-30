98ed10db538d26feade83f25efbe496dd7f266d3
Sabrina Charania <sabrinac@bu.edu>
2018-10-30 14:09:54 -0400

    Prototype updates
    - changes to app.py
    - add request_example.py
    - front end chanfes

--- client/app.py
+++ client/app.py
@@ -5,7 +5,7 @@ Description: A simple web application that connects to the sakila database and i
 '''
 
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-
+from API_request import *
 app = Flask(__name__)
 
 
@@ -16,7 +16,36 @@ def index():
     if request.method == 'GET': #if request is get (user navigated to the URL)
         return render_template('index.html')
     else:
-        return render_template('result.html')
+        event1 = {
+            "name" : str(request.form['event1_name']),
+            "start": str(request.form['event1_start']),
+            "end" : str(request.form['event1_end']),
+            "location" : str(request.form['location1'])
+        }
+        event2 = {
+            "name" : str(request.form['event2_name']),
+            "start": str(request.form['event2_start']),
+            "end" : str(request.form['event2_end']),
+            "location" : str(request.form['location2'])
+        }
+        event3 = {
+            "name" : str(request.form['event3_name']),
+            "start": str(request.form['event3_start']),
+            "end" : str(request.form['event3_end']),
+            "location" : str(request.form['location3'])
+        }
+        event4 = {
+            "name" : str(request.form['event4_name']),
+            "start": str(request.form['event4_start']),
+            "end" : str(request.form['event4_end']),
+            "location" : str(request.form['location4'])
+        }
+        results = [dict] * 3
+        results[0] = can_I_walk_it(event1, event2)
+        results[1] = can_I_walk_it(event2, event3)
+        results[2] = can_I_walk_it(event3, event4)
+
+        return render_template('result.html', event1 = event1, event2 = event2, event3= event3, event4 = event4, results = results)
 
 #search page
 @app.route('/result/', methods=['GET','POST'])
