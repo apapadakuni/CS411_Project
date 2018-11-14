cdff6fd2ee30cbaca3943fcabd376e42f0c1d04f
Sabrina Charania <sabrinac@bu.edu>
2018-11-13 21:13:21 -0500

    more bug fixes & staged registration page
    - added register.html
    - save form data
    - added sql queries to app.py
    - added requirements.txt

--- client/app.py
+++ client/app.py
@@ -8,10 +8,28 @@ mysql = MySQL()
 app = Flask(__name__)
 
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = '' #INSERT YOUR MYSQL PASSWORD
+app.config['MYSQL_DATABASE_PASSWORD'] = 'cs460iscool' #INSERT YOUR MYSQL PASSWORD
 app.config['MYSQL_DATABASE_DB'] = 'cs411_db'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor()
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET': #if request is get (user navigated to the URL)
+        return render_template('register.html')
+    else:
+        email=request.form.get('email')
+        password=request.form.get('password')
+        # age=request.form.get('age')
+        # height = request.form.get('height')
+        # weight= request.form.get('weight')
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO User_Accounts (email, password) VALUES ('{0}', '{1}')".format(email, password))
+        conn.commit()
+        return render_template('index.html', message="Successfully registered! Welcome :)")
 
 #main page of website
 #default method is only GET
@@ -20,29 +38,46 @@ def index():
     if request.method == 'GET': #if request is get (user navigated to the URL)
         return render_template('index.html')
     else:
+        event1_name = request.form['event1_name']
+        event1_start = request.form['event1_start']
+        event1_end =  request.form['event1_end']
+        location1 =  request.form['location1']
+        event2_name = request.form['event2_name']
+        event2_start = request.form['event2_start']
+        event2_end =  request.form['event2_end']
+        location2 =  request.form['location2']
+        event3_name = request.form['event3_name']
+        event3_start = request.form['event3_start']
+        event3_end =  request.form['event3_end']
+        location3 =  request.form['location3']
+        event4_name = request.form['event4_name']
+        event4_start = request.form['event4_start']
+        event4_end =  request.form['event4_end']
+        location4 =  request.form['location4']
+
         event1 = {
-            "name" : str(request.form['event1_name']),
-            "start": str(request.form['event1_start']),
-            "end" : str(request.form['event1_end']),
-            "location" : str(request.form['location1'])
+            "name" : str(event1_name),
+            "start": str(event1_start),
+            "end" : str(event1_end),
+            "location" : str(location1)
         }
         event2 = {
-            "name" : str(request.form['event2_name']),
-            "start": str(request.form['event2_start']),
-            "end" : str(request.form['event2_end']),
-            "location" : str(request.form['location2'])
+            "name" : str(event2_name),
+            "start": str(event2_start),
+            "end" : str(event2_end),
+            "location" : str(location2)
         }
         event3 = {
-            "name" : str(request.form['event3_name']),
-            "start": str(request.form['event3_start']),
-            "end" : str(request.form['event3_end']),
-            "location" : str(request.form['location3'])
+            "name" : str(event3_name),
+            "start": str(event3_start),
+            "end" : str(event3_end),
+            "location" : str(location3)
         }
         event4 = {
-            "name" : str(request.form['event4_name']),
-            "start": str(request.form['event4_start']),
-            "end" : str(request.form['event4_end']),
-            "location" : str(request.form['location4'])
+            "name" : str(event4_name),
+            "start": str(event4_start),
+            "end" : str(event4_end),
+            "location" : str(location4)
         }
         results = [dict] * 3
         results[0] = can_I_walk_it(event1, event2)
